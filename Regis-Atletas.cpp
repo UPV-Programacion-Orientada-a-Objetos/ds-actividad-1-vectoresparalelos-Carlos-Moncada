4bbe91bbff9a2d227d23c2cdef7f04275118f2e2
Carlos-Moncada <2430039@upv.edu.mx>
2025-09-22 16:12:49 -0600

    Segundo avance mejora considerable en el menu registro completo de los aletas y mostrado su informacion, tambien agregue la validacion del menu de opciones y una opcion adcional de salir del programa

--- Regis-Atletas.cpp
+++ Regis-Atletas.cpp
@@ -1,48 +1,63 @@
 #include <iostream>
+#include <string>
 
 int main() {
-    int nombrepais[5];
-    int paisorigen[5];
-    int disiplina[5];
-    int genero[5];
+    std::string nombre[5];
+    std::string paisorigen[5];
+    std::string disiplina[5];
+    std::string genero[5];
     int medallas[5];
+    int numAtletas = 0;  
+    int opcion;
 
+    do {
     std::cout << "Registro de Atletas" << std::endl;
     std::cout << "menu de opciones" << std::endl;
     std::cout << "1. Ingresar Atleta" << std::endl;
     std::cout << "2. Mostrar Atletas" << std::endl;
     std::cout << "3. Salir" << std::endl;
+    std::cout << "Seleccione una opcion: ";
 
-    int opcion;
+    std::cin >> opcion;
+
     switch (opcion)
     {
     case 1:
-        std::cout << "Ingrese el nombre del pais: " << std::endl;
-        std::cin >> nombrepais[5];
+        std::cout << "Ingrese el nombre del atleta: " << std::endl;
+        std::cin >> nombre[numAtletas];
         std::cout << "Ingrese el pais de origen: " << std::endl;
-        std::cin >> paisorigen[5];
+        std::cin >> paisorigen[numAtletas];
         std::cout << "Ingrese la disiplina: " << std::endl;
-        std::cin >> disiplina[5];
+        std::cin >> disiplina[numAtletas];
         std::cout << "Inglrese el genero: " << std::endl;
-        std::cin >> genero[5];
+        std::cin >> genero[numAtletas];
         std::cout << "Ingrese la cantidad de medallas: " << std::endl;
-        std::cin >> medallas[5];
+        std::cin >> medallas[numAtletas];
         std::cout << "Atleta registrado con exito" << std::endl;
+        numAtletas++;
         break;
-    case 2:
-        std::cout << "Mostrar Atletas" << std::endl;
-        std::cout << "1. Nombre del pais" << std::endl;
-        std::cout << "2. Pais de origen" << std::endl;
-        std::cout << "3. Disiplina" << std::endl;
-        std::cout << "4. Genero" << std::endl;
-        std::cout << "5. Cantidad de medallas" << std::endl;
+        case 2:
+        std::cout << "=== Atletas Registrados ===" << std::endl;
+        for(int i = 0; i < numAtletas; i++) {
+            std::cout << "Atleta " << (i+1) << ":" << std::endl;
+            std::cout << "Nombre: " << nombre[i] << std::endl;
+            std::cout << "Pais: " << paisorigen[i] << std::endl;
+            std::cout << "Disciplina: " << disiplina[i] << std::endl;
+            std::cout << "Genero: " << genero[i] << std::endl;
+            std::cout << "Medallas: " << medallas[i] << std::endl;
+            std::cout << "-------------------" << std::endl;
+        }
         break;
     case 3:
-        /* code */
+        std::cout << "Saliendo del programa." << std::endl;
         break;
     default:
-        break;    default:
+        std::cout << "Opcion no valida. Por favor intente de nuevo." << std::endl;
         break;
+    }
+    } while (opcion != 3);
+    {
+        std::cout << "Saliendo del programa." << std::endl;
     }
     return 0;
 
